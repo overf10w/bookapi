9095c256e56ecd65a6b09e7c885092763acdb4fa
pwnzr <pvvner@yandex.ru>
2017-04-18 02:58:54 +0300

    Code clean up & injecting our model; Sep of concerns (create bookRoutes & inject Book into it);

---
+++ Routes/bookRoutes.js
@@ -0,0 +1,43 @@
+var express = require('express');
+
+var routes = function(Book) {
+    var bookRouter = express.Router();
+
+    bookRouter.route('/Books')
+        .post(function(req, res) {
+            // req.body is body of POST method. it is populated via bodyParser
+            var book = new Book(req.body);
+
+            book.save();
+            res.status(201).send(book);
+        })
+        .get(function (req, res) {
+            // localhost/api/books?genre=Fiction ->>> req.query = { genre: 'Fiction' }
+            var query = {};
+            // allow only genre filtering
+            if (req.query.genre) {
+                query.genre = req.query.genre;
+            }
+            // Book is instance of Book schema that's hooked up to mongoose & mongodb
+            Book.find(query, function (err, books) {
+                if (err)
+                    res.status(500).send(err);
+                else
+                    res.json(books);
+            });
+        });
+
+    bookRouter.route('/Books/:bookId')
+        .get(function (req, res) {
+            Book.findById(req.params.bookId, function (err, book) {
+                if (err) 
+                    res.status(500).send(err);
+                else
+                    res.json(book);
+            });
+        });
+    
+    return bookRouter;
+};
+
+module.exports = routes;
--- app.js
+++ app.js
@@ -11,48 +11,13 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// we're executing this func in order to return bookRouter from it
+var bookRouter = require('./Routes/bookRoutes')(Book);
+
 // good way to handle multiple routes is to use router (instead of app.get())
-var bookRouter = express.Router();
-
-bookRouter.route('/Books')
-    .post(function(req, res) {
-        // req.body is body of POST method. it is populated via bodyParser
-        var book = new Book(req.body);
-
-        book.save();
-        res.status(201).send(book);
-    })
-    .get(function (req, res) {
-        // localhost/api/books?genre=Fiction ->>> req.query = { genre: 'Fiction' }
-        var query = {};
-        // allow only genre filtering
-        if (req.query.genre) {
-            query.genre = req.query.genre;
-        }
-        // Book is instance of Book schema that's hooked up to mongoose & mongodb
-        Book.find(query, function (err, books) {
-            if (err)
-                res.status(500).send(err);
-            else
-                res.json(books);
-        });
-    });
-
-bookRouter.route('/Books/:bookId')
-    .get(function (req, res) {
-        Book.findById(req.params.bookId, function (err, book) {
-            if (err) 
-                res.status(500).send(err);
-            else
-                res.json(book);
-        });
-        res.json(responseJson);
-    });
-
 app.use('/api', bookRouter);
 
 app.get('/', function (req, res) {
